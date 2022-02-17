c266e67fa802ffc878281dc320dd7b7098c56eeb
zadykian <zadykian@outlook.com>
2022-02-17 13:09:44 +0300

    initial commit

---
+++ src/FileSignature.App/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FileSignature.App;
+
+/// <summary>
+/// Application entry point. 
+/// </summary>
+internal static class Program
+{
+	/// <summary>
+	/// Entry point method. 
+	/// </summary>
+	private static void Main(string[] args) => Environment.Exit(0);
+}
