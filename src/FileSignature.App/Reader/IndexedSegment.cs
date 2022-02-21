9dd5fd3454fc2904bd7c4d7cb6e7df119368db92
zadykian <zadykian@outlook.com>
2022-02-21 19:34:01 +0300

    IndexedSegment

---
+++ src/FileSignature.App/Reader/IndexedSegment.cs
@@ -0,0 +1,46 @@
+using System.Buffers;
+
+namespace FileSignature.App.Reader;
+
+/// <summary>
+/// Indexed (zero-based) array segment.
+/// </summary>
+/// <param name="Index">
+/// Zero-based index.
+/// </param>
+/// <param name="Content">
+/// Content.
+/// </param>
+internal readonly record struct IndexedSegment(uint Index, ArraySegment<byte> Content) : IDisposable
+{
+	/// <summary>
+	/// Create new <see cref="IndexedSegment"/> instance.
+	/// </summary>
+	/// <param name="index">
+	/// Zero-based index.
+	/// </param>
+	/// <param name="blockSize">
+	/// Size of <see cref="Content"/>.
+	/// </param>
+	public IndexedSegment(uint index, Memory blockSize) : this(index, Rent(blockSize))
+	{
+	}
+
+	/// <inheritdoc />
+	public override string ToString() => $"{Index:D8}: {Convert.ToHexString(Content)}";
+
+	/// <inheritdoc />
+	/// <remarks>
+	/// Return underlying array to <see cref="ArrayPool{Byte}"/>.
+	/// </remarks>
+	public void Dispose() => ArrayPool<byte>.Shared.Return(Content.Array!);
+
+	/// <summary>
+	/// Rent <see cref="ArraySegment{Byte}"/> from <see cref="ArrayPool{Byte}"/>.
+	/// </summary>
+	private static ArraySegment<byte> Rent(Memory blockSize)
+		=> new(
+			ArrayPool<byte>.Shared.Rent((int)blockSize.TotalBytes),
+			offset: 0,
+			count: (int)blockSize.TotalBytes);
+}
